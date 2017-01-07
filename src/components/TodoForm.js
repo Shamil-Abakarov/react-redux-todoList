d16e0e2739c1941a9da12b940e9469c63c561eef
Shamil Abakarov <abakarov-shamil@mail.ua>
2017-01-07 21:36:48 +0200

    create TodoForm

---
+++ src/components/TodoForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+	render(){
+		return(
+			<div>
+				<form>
+					<textarea />
+					<button type="submit">ADD TODO</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default TodoForm;
