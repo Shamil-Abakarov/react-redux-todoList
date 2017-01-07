60634e1b07a3a72eb7af6899311334b0bee2ff5e
Shamil Abakarov <abakarov-shamil@mail.ua>
2017-01-07 22:28:28 +0200

    import TodoList component

--- src/components/TodoContent.js
+++ src/components/TodoContent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 class TodoContent extends Component {
 	render(){
 		return(
 			<div>
 				<TodoForm />
+				<TodoList />
 			</div>
 		);
 	}
